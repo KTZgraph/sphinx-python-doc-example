2a94831a19106cb04a2ed0e70580b9f541aa6f26
pawlaczyk <pawlaczyk@g.pl>
2020-10-17 22:12:36 +0200

    addig funtions to document

---
+++ docs/example1.py
@@ -0,0 +1,36 @@
+def func1(arg1, arg2):
+    """
+    This function takes two arguments, sets the first equal to second, then returns the new first argument. Pointless
+
+    :param arg1: Some value
+    :param arg2: Another value
+
+    :return: arg1
+    """
+    arg1 = arg2
+    return arg1
+
+
+# def func11(arg1, arg2):
+#     """
+#     Func descript ion google style
+#     Args:
+#         arg1: value
+#         arg2: value
+#
+#     Returns:
+#         arg1
+#
+#     """
+#     arg1 = arg2
+#     return arg1
+
+def func2():
+    """
+    Function with no arguments returns None
+
+    :return: None
+
+    """
+    return None
+
